bac0d72cc7152079138b77c5966918ff57f4a6b8
Daksh <daksh@technowolf.in>
2018-08-28 01:20:52 +0530

    Added new style usage codes
    Signed-off-by: Daksh <daksh@technowolf.in>

--- assets/js/style-codes.js
+++ assets/js/style-codes.js
@@ -37,6 +37,95 @@ function getStyle(styleCode) {
                 "&lt;/figure&gt;";
             break;
         }
+        case 4: {
+            style = "&lt;figure class=\"style-four\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\" /&gt;\n" +
+                "    &lt;figcaption&gt;\n" +
+                "        &lt;div&gt;&lt;i class=\"fas fa-plus\"&gt;&lt;/i&gt;&lt;/div&gt;\n" +
+                "    &lt;/figcaption&gt;\n" +
+                "    &lt;a href=\"#\"&gt;&lt;/a&gt;\n" +
+                "&lt;/figure&gt;";
+            break;
+        }
+        case 5: {
+            style = "&lt;figure class=\"style-five\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\"  /&gt;\n" +
+                "    &lt;i class=\"icon-star\"&gt;&lt;/i&gt;\n" +
+                "    &lt;a href=\"#\"&gt;&lt;/a&gt;\n" +
+                "&lt;/figure&gt;";
+            break;
+        }
+        case 6: {
+            style = "&lt;figure class=\"style-six\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\"  /&gt;\n" +
+                "    &lt;div class=\"icons\"&gt;\n" +
+                "        &lt;a href=\"#\"&gt;&lt;i class=\"fab fa-facebook\"&gt;&lt;/i&gt;&lt;/a&gt;\n" +
+                "        &lt;a href=\"#\"&gt;&lt;i class=\"fab fa-twitter\"&gt;&lt;/i&gt;&lt;/a&gt;\n" +
+                "        &lt;a href=\"#\"&gt;&lt;i class=\"fab fa-telegram\"&gt;&lt;/i&gt;&lt;/a&gt;\n" +
+                "    &lt;/div&gt;\n" +
+                "&lt;/figure&gt;";
+            break;
+        }
+        case 7: {
+            style = "";
+            break;
+        }
+        case 8: {
+            style = "";
+            break;
+        }
+        case 9: {
+            style = "";
+            break;
+        }
+        case 10: {
+            style = "";
+            break;
+        }
+        case 11: {
+            style = "";
+            break;
+        }
+        case 12: {
+            style = "";
+            break;
+        }
+        case 13: {
+            style = "";
+            break;
+        }
+        case 14: {
+            style = "";
+            break;
+        }
+        case 15: {
+            style = "";
+            break;
+        }
+        case 16: {
+            style = "";
+            break;
+        }
+        case 17: {
+            style = "";
+            break;
+        }
+        case 18: {
+            style = "";
+            break;
+        }
+        case 19: {
+            style = "";
+            break;
+        }
+        case 20: {
+            style = "";
+            break;
+        }
+        case 21: {
+            style = "";
+            break;
+        }
         default: {
             style = "you mad bruh?";
         }
