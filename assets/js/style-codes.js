abc89822267ed054ab9068d53ec7ed18d83f39ee
Daksh <daksh@technowolf.in>
2018-08-28 22:46:30 +0530

    added 6 new usage code
    Signed-off-by: Daksh <daksh@technowolf.in>

--- assets/js/style-codes.js
+++ assets/js/style-codes.js
@@ -67,27 +67,71 @@ function getStyle(styleCode) {
             break;
         }
         case 7: {
-            style = "";
+            style = "&lt;figure class=\"style-seven\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\"  /&gt;\n" +
+                "    &lt;div class=\"icons\"&gt;\n" +
+                "        &lt;a href=\"#\"&gt;&lt;i class=\"fab fa-facebook\"&gt;&lt;/i&gt;&lt;/a&gt;\n" +
+                "        &lt;a href=\"#\"&gt;&lt;i class=\"fab fa-twitter\"&gt;&lt;/i&gt;&lt;/a&gt;\n" +
+                "        &lt;a href=\"#\"&gt;&lt;i class=\"fab fa-telegram\"&gt;&lt;/i&gt;&lt;/a&gt;\n" +
+                "    &lt;/div&gt;\n" +
+                "&lt;/figure&gt;";
             break;
         }
         case 8: {
-            style = "";
+            style = "&lt;figure class=\"style-eight\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\"  /&gt;\n" +
+                "    &lt;figcaption&gt;\n" +
+                "        &lt;div&gt;&lt;i class=\"fas fa-heart\"&gt;&lt;/i&gt;&lt;/div&gt;\n" +
+                "    &lt;/figcaption&gt;\n" +
+                "    &lt;a href=\"#\"&gt;&lt;/a&gt;\n" +
+                "&lt;/figure&gt;";
             break;
         }
         case 9: {
-            style = "";
+            style = "&lt;figure class=\"style-nine\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\"  /&gt;\n" +
+                "    &lt;figcaption&gt;\n" +
+                "        &lt;h2&gt;Heading&lt;/h2&gt;\n" +
+                "        &lt;h3&gt;Another Heading&lt;/h3&gt;\n" +
+                "        &lt;p&gt;A paragraph&lt;/p&gt;\n" +
+                "    &lt;/figcaption&gt;\n" +
+                "    &lt;a href=\"#\"&gt;&lt;/a&gt;\n" +
+                "&lt;/figure&gt;";
             break;
         }
         case 10: {
-            style = "";
+            style = "&lt;figure class=\"style-ten\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\"  /&gt;\n" +
+                "    &lt;figcaption&gt;\n" +
+                "        &lt;h3&gt;Header&lt;/h3&gt;\n" +
+                "        &lt;p&gt;Paragraph&lt;/p&gt;\n" +
+                "    &lt;/figcaption&gt;\n" +
+                "    &lt;i class=\"fas fa-plus\"&gt;&lt;/i&gt;\n" +
+                "    &lt;a href=\"#\"&gt;&lt;/a&gt;\n" +
+                "&lt;/figure&gt;";
             break;
         }
         case 11: {
-            style = "";
+            style = "&lt;figure class=\"style-eleven\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\" /&gt;\n" +
+                "    &lt;figcaption&gt;\n" +
+                "        &lt;h2&gt;Heading&lt;/h2&gt;\n" +
+                "        &lt;h3&gt;One More&lt;/h3&gt;\n" +
+                "        &lt;p&gt;A paragraph&lt;/p&gt;\n" +
+                "    &lt;/figcaption&gt;\n" +
+                "    &lt;a href=\"#\"&gt;&lt;/a&gt;\n" +
+                "&lt;/figure&gt;";
             break;
         }
         case 12: {
-            style = "";
+            style = "&lt;figure class=\"style-twelve\"&gt;\n" +
+                "    &lt;img src=\"/path/to/image\" /&gt;\n" +
+                "    &lt;figcaption&gt;\n" +
+                "        &lt;h3&gt;Heading&lt;/h3&gt;\n" +
+                "        &lt;p&gt;Paragraph&lt;/p&gt;\n" +
+                "    &lt;/figcaption&gt;\n" +
+                "    &lt;a href=\"#\"&gt;&lt;/a&gt;\n" +
+                "&lt;/figure&gt;";
             break;
         }
         case 13: {
